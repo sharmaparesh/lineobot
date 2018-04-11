e5c67ca1bfe76e16faa1b3e9e1c4e29fd79c70ba
paresharma <paresh.brahm@gmail.com>
2018-04-11 18:40:18 +0900

    Post it

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'hooks#index'
-  get '/callback', to: 'hooks#callback'
+  post '/callback', to: 'hooks#callback'
   resources :hooks, only: [:index, :show, :destroy]
 end
