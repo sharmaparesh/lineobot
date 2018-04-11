e1d713f01a9960aab5478dc2c24d57ed6e244f37
sharmaparesh <paresh@tripla.jp>
2018-04-11 18:09:06 +0900

    Add callback

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,5 +1,5 @@
 class HooksController < ApplicationController
-  before_action :set_hook, only: [:show, :edit, :update, :destroy]
+  before_action :set_hook, only: [:show, :destroy]
 
   # GET /hooks
   # GET /hooks.json
@@ -12,43 +12,11 @@ class HooksController < ApplicationController
   def show
   end
 
-  # GET /hooks/new
-  def new
-    @hook = Hook.new
-  end
-
-  # GET /hooks/1/edit
-  def edit
-  end
-
-  # POST /hooks
-  # POST /hooks.json
-  def create
-    @hook = Hook.new(hook_params)
-
-    respond_to do |format|
-      if @hook.save
-        format.html { redirect_to @hook, notice: 'Hook was successfully created.' }
-        format.json { render :show, status: :created, location: @hook }
-      else
-        format.html { render :new }
-        format.json { render json: @hook.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /hooks/1
-  # PATCH/PUT /hooks/1.json
-  def update
-    respond_to do |format|
-      if @hook.update(hook_params)
-        format.html { redirect_to @hook, notice: 'Hook was successfully updated.' }
-        format.json { render :show, status: :ok, location: @hook }
-      else
-        format.html { render :edit }
-        format.json { render json: @hook.errors, status: :unprocessable_entity }
-      end
-    end
+  def callback
+    h = Hook.new
+    h.payload = params
+    h.save
+    head :ok
   end
 
   # DELETE /hooks/1
@@ -62,13 +30,8 @@ class HooksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_hook
-      @hook = Hook.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def hook_params
-      params.require(:hook).permit(:payload)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_hook
+    @hook = Hook.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :hooks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'hooks#index'
+  get '/callback', to: 'hooks#callback'
+  resources :hooks, only: [:index, :show, :destroy]
 end
