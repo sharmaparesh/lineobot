366918b0eda9f9549a6e47044c7a51b045e40c61
paresharma <paresh.brahm@gmail.com>
2018-04-16 16:53:33 +0900

    Auto reply

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,3 +1,5 @@
+require 'line/bot'
+
 class HooksController < ApplicationController
   before_action :set_hook, only: [:show, :destroy]
   protect_from_forgery except: :callback
@@ -14,9 +16,34 @@ class HooksController < ApplicationController
   end
 
   def callback
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      head :bad_request and return
+    end
+
     h = Hook.new
     h.payload = params
     h.save
+
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: "This is in response to:: #{event.message['text']}"
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      end
+    end
     head :ok
   end
 
@@ -34,5 +61,12 @@ class HooksController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_hook
     @hook = Hook.find(params[:id])
+  end
+
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    end
   end
 end
